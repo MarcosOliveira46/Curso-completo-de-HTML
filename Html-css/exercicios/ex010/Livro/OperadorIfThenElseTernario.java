9c608429bd1c42220b9cb5941abfbbc6ef33892a
MarcosOliveira46 <markiinjn@gmail.com>
2021-09-13 17:08:45 -0300

    Finalizando o capítulo 10

---
+++ Html-css/exercicios/ex010/Livro/OperadorIfThenElseTernario.java
@@ -0,0 +1,14 @@
+public class OperadorIfThenElseTernario{
+
+
+	public static void main (String [] args){
+
+		int a = 0;
+		String b;
+
+		b = a == 12 ? "12 não é zero" : "zero é zero";
+		System.out.print(b);
+
+	}
+
+}
